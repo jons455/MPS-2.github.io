513aa596f7a4203067ad002404e683dfd9878533
paulinadsmn <paulinadismann@yahoo.de>
2020-12-09 16:23:00 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/MPS-2/MPS-2.github.io into main""
    This reverts commit 2d04dc3bf71d7ac61aae21419c1f577379facec1.

--- test3.js
+++ test3.js
@@ -3,10 +3,6 @@
 //Bootstrap anpassen required
 document.addEventListener('DOMContentLoaded', function() {
 
-    var modals = document.querySelectorAll('.modal');
-    M.Modal.init(modals);
-  
-    var items = document.querySelectorAll('.collapsible');
-    M.Collapsible.init(items);
+    
   
   });
