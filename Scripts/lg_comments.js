ca2035d2b036b6ef76ff36e2090a494e9a7d1dc3
AndreaKre <andrea.krestakova@fau.de>
2021-01-20 20:37:14 +0100

    comments mobile

--- Scripts/lg_comments.js
+++ Scripts/lg_comments.js
@@ -13,7 +13,7 @@ const setupLG = (data) => {
         const post = doc.data();
         const tr = `
         <tr>
-            <td style="text-align:center; word-wrap: break-word; word-break:break-word; width:20%;">
+            <td style="text-align:center;width:20%;">
                 <div style="color:blue;">${post.user}</div>
                 <div>${post.time.toDate().toLocaleDateString()}</div>
                 <div>${post.time.toDate().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})}</div>
